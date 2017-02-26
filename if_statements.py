8603c467c2904015570359cca7340bdd5c6b8290
stephen <lonelytreesl@yahoo.com>
2017-02-26 15:06:19 -0800

    Add initial practice if statements.

---
+++ if_statements.py
@@ -0,0 +1,11 @@
+if x == y:
+    print "x is equal to y!"
+if a_variable > 2:
+    print "The variable is greater than 2"
+if player_name == "Bob":
+    print "Hello Bob!"
+
+if player_name == "Bob":
+    print "Hi Bob!"
+else:
+    print "Hey! You're not Bob!"
