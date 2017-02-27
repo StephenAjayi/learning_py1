d9663ddb82819ef756e10bd1261681a122e880ec
stephen <lonelytreesl@yahoo.com>
2017-02-26 20:46:52 -0800

    Add initial list and print statements.

---
+++ lists.py
@@ -0,0 +1,8 @@
+shopping_list = ['Milk', 'Bread', 'Cheese', 'Bow and Arrow']
+
+print shopping_list
+print shopping_list[0]
+print shopping_list[-1]
+
+if 'Milk' in shopping_list:
+    print "Oh good, you remebered the milk!"
