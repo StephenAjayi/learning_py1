387eb400afac5dcfae3f2e660c28e1406db801fe
stephen <lonelytreesl@yahoo.com>
2017-02-26 20:34:00 -0800

    Add 2nd wumpus to game.

--- wumpus_hunt.py
+++ wumpus_hunt.py
@@ -2,8 +2,9 @@ from random import choice
 
 cave_numbers = range(1,21)
 wumpus_location = choice(cave_numbers)
+wumpus_friend_location = choice(cave_numbers)
 player_location = choice(cave_numbers)
-while player_location == wumpus_location:
+while player_location == wumpus_location or player_location == wumpus_friend_location:
     player_location = choice(cave_numbers)
 
 print "Welcome to Hunt the Wumpus!"
@@ -16,6 +17,9 @@ while True:
     if (player_location == wumpus_location - 1 or player_location == wumpus_location + 1):
         print "I smell a wumpus!"
 
+    if (player_location == wumpus_friend_location - 1 or player_location == wumpus_friend_location + 1):
+        print "I smell a even stinkier wumpus!"
+
     print "Which cave next?"
     player_input = raw_input(">")
     if (not player_input.isdigit() or int(player_input) not in cave_numbers):
@@ -23,6 +27,6 @@ while True:
 
     else:
         player_location = int(player_input)
-        if player_location == wumpus_location:
+        if player_location == wumpus_location or player_location == wumpus_friend_location:
             print "Aargh! You got eaten by a wumpus!"
             break
