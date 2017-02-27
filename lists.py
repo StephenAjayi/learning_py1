d72f8e73a06c8cab1d27542bf5b5e517104719e5
stephen <lonelytreesl@yahoo.com>
2017-02-26 21:04:42 -0800

    Add new list and additional list to comtain both lists.

--- lists.py
+++ lists.py
@@ -1,8 +1,24 @@
-shopping_list = ['Milk', 'Bread', 'Cheese', 'Bow and Arrow']
+supermarket_list = ['Milk', 'Bread', 'Cheese']
 
-print shopping_list
-print shopping_list[0]
-print shopping_list[-1]
+print supermarket_list
+print supermarket_list[0]
+print supermarket_list[-1]
 
-if 'Milk' in shopping_list:
+if 'Milk' in supermarket_list:
     print "Oh good, you remebered the milk!"
+
+wumpus_r_us_list = ['Bow and Arrow', 'Lantern', 'Wumpus B Gone']
+
+print wumpus_r_us_list
+print wumpus_r_us_list[0]
+print wumpus_r_us_list[-1]
+
+if 'Bow and Arrow' in wumpus_r_us_list:
+    print "Oh good, you remembered the bow and arrow!"
+
+wumpus_r_us_list.append('Rope')
+wumpus_r_us_list.remove('Wumpus B Gone')
+
+shopping_lists = [supermarket_list, wumpus_r_us_list]
+
+print shopping_lists
